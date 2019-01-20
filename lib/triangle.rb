4dc6895ba5f655cebbc8ab33b4133b8667b2612f
Steve Albonico <salbonico@gmail.com>
2019-01-20 18:27:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,20 @@ class Triangle
   # write code here
  attr_accessor :equilateral :isosceles :scalene
 
+def initialize(l1,l2,l3)
+@l1 = l1
+@l2 = l2
+@l3 = l3
 
-  
+end
+
+
+
+
+def kind
+if @l2 == @l1 && @l3 == @l1
+  return :equilateral
+
+end
+
 end
